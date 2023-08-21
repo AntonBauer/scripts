6f0aaa45fff05825cee9318417a683563def4945
Anton Bauer <bauer_av@proton.me>
2023-08-21 19:12:42 +0200

    Add nvim config clone, add trackball setup script

--- install-scripts/nvim-install.sh
+++ install-scripts/nvim-install.sh
@@ -6,8 +6,11 @@ wget https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.
 tar xzvf nvim-linux64.tar.gz
 sudo mv ./nvim-linux64 /usr/local/bin/ 
 
-# Add nvim path to PATH
+# add nvim path to PATH
 echo "if [ -d /usr/local/bin/nvim-linux64 ]; then PATH = $PATH:/usr/local/bin/nvim-linux64/bin fi" >> ~/.bashrc
 
 # set nvim as default editor
 sudo update-alternatives --install /usr/bin/editor/ editor /usr/local/bin/nvim-linux64/bin/nvim 100
+
+# clone nvim config
+git clone git@github.com:AntonBauer/nvim_config.git ~/.config/nvim
