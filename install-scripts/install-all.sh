626fb33abd8b676b70212311616d3cecf77311d2
Anton Bauer <bauer_av@proton.me>
2023-08-25 21:17:08 +0200

    Add github cli installation

--- install-scripts/install-all.sh
+++ install-scripts/install-all.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 sudo apt-get update
+sudo apt update
+
+./curl-install.sh
 ./pass-install.sh
 ./git-install.sh
+./github-cli-install.sh
 ./ripgrep-install.sh
 ./nvim-install.sh
 ./proton-vpn-install.sh
